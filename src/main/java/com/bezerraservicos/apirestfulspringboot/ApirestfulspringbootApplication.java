6a7e2f67bc381a7066f0e26b03005106459e2449
Ricardo Santos Bezerra <rico.bezerra@hotmail.com>
2021-05-03 23:19:17 -0300

    Projeto criado.

---
+++ src/main/java/com/bezerraservicos/apirestfulspringboot/ApirestfulspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.bezerraservicos.apirestfulspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestfulspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestfulspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bezerraservicos/apirestfulspringboot/ApirestfulspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezerraservicos.apirestfulspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestfulspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
