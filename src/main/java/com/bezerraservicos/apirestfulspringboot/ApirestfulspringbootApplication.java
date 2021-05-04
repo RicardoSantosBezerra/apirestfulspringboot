fcb9bbaafd38c807dddf987b763be9b5d6d25bf4
Ricardo Santos Bezerra <rico.bezerra@hotmail.com>
2021-05-04 16:11:58 -0300

    Criando operação de instanciação.

--- src/main/java/com/bezerraservicos/apirestfulspringboot/ApirestfulspringbootApplication.java
+++ src/main/java/com/bezerraservicos/apirestfulspringboot/ApirestfulspringbootApplication.java
@@ -1,13 +1,31 @@
 package com.bezerraservicos.apirestfulspringboot;
 
+import com.bezerraservicos.apirestfulspringboot.domain.Categoria;
+import com.bezerraservicos.apirestfulspringboot.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ApirestfulspringbootApplication {
+public class ApirestfulspringbootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestfulspringbootApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(0, "Informática");
+		Categoria cat2 = new Categoria(0, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
